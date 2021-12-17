5194446356ce98f8cce04d9a1393631df68ec731
Alex Boxell <alexjboxell@gmail.com>
2021-12-17 14:46:33 +0000

    added map with custom markers

--- app/controllers/hills_controller.rb
+++ app/controllers/hills_controller.rb
@@ -2,6 +2,13 @@ class HillsController < ApplicationController
 
   def index
     @hills = Hill.all
+    @markers = @hills.map do |hill|
+      {
+        lat: hill.latitude,
+        lng: hill.longitude,
+        image_url: helpers.asset_url("mountain.svg")
+      }
+    end
   end
 
   def show
