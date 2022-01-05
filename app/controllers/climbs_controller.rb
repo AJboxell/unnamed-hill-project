9de4495c1e817554b3936cd8ab49f240362a27e5
Alex Boxell <alexjboxell@gmail.com>
2022-01-05 12:01:52 +0000

    added basic page for climbs

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,5 +1,9 @@
 class ClimbsController < ApplicationController
 
+  def index
+    @climbs = Climb.where(user: current_user)
+  end
+
   def create
     @climb = Climb.new(climb_params)
     @climb.user = current_user
